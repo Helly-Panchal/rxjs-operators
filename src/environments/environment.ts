d9780c36dd7ba8d19cd36ecee703cbe0d007ba57
Helly-Panchal <helly.panchal@bacancy.com>
2023-06-13 14:23:38 +0530

    Setup firebase and deploy application

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyDa-tWc-1oqhc-9QAqQtiA0UF7gI0H5Naw",
+    authDomain: "rxjs-9e789.firebaseapp.com",
+    projectId: "rxjs-9e789",
+    storageBucket: "rxjs-9e789.appspot.com",
+    messagingSenderId: "477192397400",
+    appId: "1:477192397400:web:a3b9235f134aacd504602d"
+  }
+};
