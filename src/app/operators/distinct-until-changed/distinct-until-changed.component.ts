bc2a750f5316add6efdbd89ae5c9fd1ef1b68683
Helly-Panchal <helly.panchal@bacancy.com>
2023-06-13 11:30:50 +0530

    Implemented DistinctUntilChanged operator

--- src/app/operators/distinct-until-changed/distinct-until-changed.component.ts
+++ src/app/operators/distinct-until-changed/distinct-until-changed.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, distinctUntilChanged, of, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-distinct-until-changed',
   templateUrl: './distinct-until-changed.component.html',
   styleUrls: ['./distinct-until-changed.component.scss']
 })
-export class DistinctUntilChangedComponent {
+export class DistinctUntilChangedComponent implements OnInit, OnDestroy {
+
+  public sub$ = new Subject();
+  public container: number[] = [];
+  public observable$ = of(1, 1, 1, 2, 3, 3, 2, 4, 4, 5, 6, 6, 5, 7, 7, 9);
+
+  ngOnInit(): void {
+    this.observable$.pipe(takeUntil(this.sub$), distinctUntilChanged()).subscribe((res) => {
+      this.container.push(res);
+    })
+  }
+  ngOnDestroy(): void {
+    this.sub$.next(0);
+    this.sub$.unsubscribe();
+  }
 
 }
